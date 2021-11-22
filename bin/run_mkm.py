840099f6abe8fd0e227a1dd3ba06e0cf75a79f16
Austen Bernardi <a.bernardi@utah.edu>
2021-11-22 10:26:07 -0700

    formatting

--- bin/run_mkm.py
+++ bin/run_mkm.py
@@ -19,9 +19,9 @@ def main():
 	for j in range(N):
 		mkm_sys = MKM_sys(configs[j])
 		print('System {:d} Ion flows (ions/ms):'.format(j))
-		print('Bio Cl: {:4e}'.format(mkm_sys.ion_flows[0][0]))
-		print('Bio  H: {:4e}'.format(mkm_sys.ion_flows[0][1]))
-		print('Opp Cl: {:4e}'.format(mkm_sys.ion_flows[1][0]))
-		print('Opp  H: {:4e}\n'.format(mkm_sys.ion_flows[1][1]))
+		print('Bio Cl: {:>11.4e}'.format(mkm_sys.ion_flows[0][0]))
+		print('Bio  H: {:>11.4e}'.format(mkm_sys.ion_flows[0][1]))
+		print('Opp Cl: {:>11.4e}'.format(mkm_sys.ion_flows[1][0]))
+		print('Opp  H: {:>11.4e}\n'.format(mkm_sys.ion_flows[1][1]))
 
 main()
